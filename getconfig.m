782b2e86e1c4ff28198d8f39c9fb36ba24a12b03
chengy35 <chengy35@mail2.sysu.edu.cn>
2017-06-05 17:20:32 +0800

    s

--- getconfig.m
+++ getconfig.m
@@ -1,9 +1,9 @@
 function [videoname, classlabel,fv_dir, w_dir, vocab_dir, descriptor_path, video_dir,actions, tr_index] = getconfig(split, DATASET)
-            fv_dir = ['~/remote/hmdb51Data/fv/feats', num2str(split)]; % Path where features will be saved
-            w_dir = ['~/remote/hmdb51Data/w/feats', num2str(split)]; % Path where features will be saved
-            vocab_dir = '~/remote/hmdb51Data/Vocab';
-            descriptor_path = '~/remote/hmdb51Data/descriptor';
-            video_dir = '~/remote/hmdb51';
-            splitdir = '~/remote/hmdb51Data/testTrainMulti_7030_splits';
+            fv_dir = ['/mnt/remote/hmdb51Data/fv/feats', num2str(split)]; % Path where features will be saved
+            w_dir = ['/mnt/remote/hmdb51Data/w/feats', num2str(split)]; % Path where features will be saved
+            vocab_dir = '/mnt/remote/hmdb51Data/Vocab';
+            descriptor_path = '/mnt/remote/hmdb51Data/descriptor';
+            video_dir = '/mnt/remote/hmdb51';
+            splitdir = '/mnt/remote/hmdb51Data/testTrainMulti_7030_splits';
             [videoname, classlabel, tr_index, ~, ~, actions]= getHmdbSplit(split,splitdir);
 end
