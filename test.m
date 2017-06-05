eedff4530b011caf178269310014766a0db8769d
chengy35 <chengy35@mail2.sysu.edu.cn>
2017-06-05 20:40:45 +0800

    s

--- test.m
+++ test.m
@@ -20,21 +20,23 @@ fprintf('getGMM \n');
 fprintf('generate Fisher Vectors \n');
 
 
-
-if ~exist(fv_dir,'dir'), mkdir(fv_dir), end
-    [path, ~, ~]=fileparts(videoname{1});
-    if ~exist(fullfile(fv_dir,path),'dir')
-        for i = 1 : numel(videoname)
-            [path, ~, ~]=fileparts(videoname{i});
-            if ~exist(fullfile(fv_dir,path), 'dir')
-                mkdir(fullfile(fv_dir,path));
-            end
-        end
-    end
-
-encodeVideos(videoname,gmm,codebook,fv_dir,descriptor_path, encode_method,video_dir);
-
 fvhogdimension = size(gmm.pcamap.hog,2)*2*size(gmm.means.hog,2);
 fvhofdimension = size(gmm.pcamap.hof,2)*2*size(gmm.means.hof,2);
 fvmbhxdimension = size(gmm.pcamap.mbhx,2)*2*size(gmm.means.mbhx,2);
 fvmbhydimension =  size(gmm.pcamap.mbhy,2)*2*size(gmm.means.mbhy,2);
+
+  if ~exist(w_dir,'dir'), mkdir(w_dir), end
+    [path, ~, ~]=fileparts(videoname{1});
+    if ~exist(fullfile(w_dir,path),'dir')
+        for i = 1 : numel(videoname)
+            [path, ~, ~]=fileparts(videoname{i});
+            if ~exist(fullfile(w_dir,path), 'dir')
+                mkdir(fullfile(w_dir,path));
+            end
+        end
+    end
+    
+    w_hog = zeros( numel(videoname),fvhogdimension*2);
+    w_hof = zeros( numel(videoname),fvhofdimension*2);
+    w_mbhx = zeros( numel(videoname),fvmbhxdimension*2);
+    w_mbhy = zeros( numel(videoname),fvmbhydimension*2);
